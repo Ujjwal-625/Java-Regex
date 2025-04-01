8b014f19568e7d3dd3f16056c2baad37d0591b92
Ujjwal <Ujjwalrawat248@gmail.com>
2025-04-01 19:25:55 +0530

    [Author:Ujjwal] MOD:ExtractLink.java ExtractDates.java

--- src/ExtractDates.java
+++ src/ExtractDates.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class ExtractDates {
     public static void main(String[] args) {
-        String regex= "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$";;
+        String regex= "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$";
 
 
         Pattern pattern=Pattern.compile(regex);
--- src/ExtractLink.java
+++ src/ExtractLink.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExtractLink {
+    public static void main(String[] args) {
+        String regex= "https?://[^\\s]+";
+
+
+        Pattern pattern=Pattern.compile(regex);
+
+
+        String sentance="Visit https://www.google.com and http://example.org for more info";
+
+
+        String [] arr=sentance.split(" ");
+
+        for(String i:arr){
+            Matcher matcher=pattern.matcher(i);
+
+            if(matcher.matches()){
+                System.out.println(i);
+            }
+        }
+
+    }
 }
